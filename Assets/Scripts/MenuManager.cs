67f14e8792971d65128a3cb1516edd3075c0c02d
Bladamz <bladamz@hotmail.com>
2019-06-04 10:02:09 +1000

    File Structure and basic scripts
    Oragnized some folders and added base scripts

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("OtherSceneName");
+    }
+}
